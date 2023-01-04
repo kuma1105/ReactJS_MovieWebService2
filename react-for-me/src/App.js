fe5d8c16b4aa08770ecc986dd01994ac5e66a513
Fancyee <74527909+kuma1105@users.noreply.github.com>
2023-01-05 00:12:08 +0900

    useState, onClick 이벤트

--- react-for-me/src/App.js
+++ react-for-me/src/App.js
@@ -1,11 +1,18 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  }
+  console.log("<App/> render");
+  // state가 변화할 때 너의 모든 Component는 다시 실행될 것이다.
   return (
     <div>
-      <h1 className={styles.title}>Welcome!!!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      {/* <Button onClick={onClick} text={"Continue"} /> */}
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
