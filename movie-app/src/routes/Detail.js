7fe32c4b8e9d5be9dd311e8d09d2aaf8a940ca8a
Fancyee <74527909+kuma1105@users.noreply.github.com>
2023-01-08 21:21:57 +0900

    useParams 사용

--- movie-app/src/routes/Detail.js
+++ movie-app/src/routes/Detail.js
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>;
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    const { id } = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json.data.movie);
+        setMovie(json.data.movie);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getMovie();
+    }, [])
+
+    return (
+        <div>
+            {loading ? (<h1>Loading...</h1>) :
+                (
+                    <div>
+                        <h1>{movie.id}</h1>
+                        <h1>{movie.rating}</h1>
+                    </div>
+                )
+            }
+
+        </div>
+    );
 }
 
 export default Detail;
