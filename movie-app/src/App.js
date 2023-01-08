abdf2615af6ac56448c620d95d89c326f9f36562
Fancyee <74527909+kuma1105@users.noreply.github.com>
2023-01-08 20:49:00 +0900

    react-router-dom Link, Router, Switch, Route 사용

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,8 +1,30 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie.js";
+
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello!</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
